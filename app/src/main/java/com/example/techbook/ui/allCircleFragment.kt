4b6f62f4d9ccc2f4ef99000796e0900525013fe9
ho2ri <ho2ri2s.github@gmail.com>
2019-10-22 13:28:44 +0900

    separate core logic to viewmodel

--- app/src/main/java/com/example/techbook/ui/allCircleFragment.kt
+++ app/src/main/java/com/example/techbook/ui/allCircleFragment.kt
@@ -1,40 +1,40 @@
 package com.example.techbook.ui
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-
 import com.example.techbook.R
 import com.example.techbook.adapter.CircleRecyclerViewAdapter
-import com.example.techbook.data.api.entity.CircleEntity
-import com.example.techbook.data.api.entity.CircleEntityResult
-import com.example.techbook.data.api.service.CircleService
-import com.example.techbook.viewmodel.CircleViewModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.techbook.data.api.service.CircleInterface
+import com.example.techbook.data.api.service.circleService
+import com.example.techbook.data.db.AppDatabase
+import kotlinx.android.synthetic.main.fragment_all_circle.*
+import java.lang.IllegalArgumentException
 
-import kotlin.concurrent.thread
+class AllCircleFragment : Fragment() {
 
-class allCircleFragment : Fragment() {
+    private val circleInterface by lazy { circleService() }
+    private val appDatabase by lazy { AppDatabase.get(requireContext()) }
+    private val viewModelFactory by lazy { Factory(circleInterface, appDatabase) }
+    private lateinit var viewModel: AllCircleViewModel
 
-    private val circleInterface by lazy { CircleService() }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(AllCircleViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_all_circle, container, false)
     }
 
@@ -42,60 +42,42 @@ class allCircleFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         setHasOptionsMenu(true)
-        activity!!.setTitle("All Book")
+        activity?.title = "All Book"
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = CircleRecyclerViewAdapter(view!!.context)
+        val adapter = CircleRecyclerViewAdapter(requireContext())
 
-        val userViewModel = ViewModelProviders.of(this).get(CircleViewModel::class.java)
-        adapter.putView(userViewModel)
-
-        val listView = view.findViewById<RecyclerView>(R.id.circleAll)
+//        adapter.putView(viewModel)
 
         val decor = DividerItemDecoration(view.context, DividerItemDecoration.VERTICAL)
-        listView.addItemDecoration(decor)
-        listView.adapter = adapter
-        listView.layoutManager = LinearLayoutManager(view.context)
+        circleList.apply {
+            this.adapter = adapter
+            this.layoutManager = LinearLayoutManager(view.context)
+            addItemDecoration(decor)
+        }
 
-        thread {
-            fetchItems(){
-                itemList -> adapter.setQiitaData(itemList.result)
+        viewModel.apply {
+            fetchAllCircle()
+            allCircle.observe(viewLifecycleOwner, Observer {
+                adapter.setQiitaData(it)
+            })
+        }
+    }
+
+    class Factory(
+        private val circleInterface: CircleInterface,
+        private val appDatabase: AppDatabase
+    ) : ViewModelProvider.Factory{
+        @Suppress("UNCHECKED CAST")
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if(modelClass.isAssignableFrom(AllCircleViewModel::class.java)){
+                return AllCircleViewModel(circleInterface, appDatabase) as T
             }
+            throw IllegalArgumentException("Unknown class name")
         }
 
     }
-
-    private fun fetchItems(callback: (CircleEntityResult) -> Unit) {
-
-        circleInterface.circleAll().enqueue(object : Callback<CircleEntityResult> {
-            override fun onFailure(call: Call<CircleEntityResult>?, t: Throwable?) {
-                Log.d("fetchItems", "response fail")
-                Log.d("fetchItems", "throwable :$t")
-            }
-
-            override fun onResponse(call: Call<CircleEntityResult>?, response: Response<CircleEntityResult>) {
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        Log.d("fetchItems", "response success")
-                        response.body()?.let {
-                            callback(it)
-                        }
-                        var items = mutableListOf<String>()
-                        /*itemRepository.getAllCircle() { itemList ->
-                            adapter.setQiitaData(itemList)
-                        }*/
-                        /*for (item in it) {
-                            items.add(item.title)
-                        }*/
-                    }
-                }
-                Log.d("fetchItems", "response code:" + response.code())
-                Log.d("fetchItems", "response errorBody:" + response.errorBody())
-            }
-        })
-    }
-
 }
--- app/src/main/java/com/example/techbook/ui/allCircleViewModel.kt
+++ app/src/main/java/com/example/techbook/ui/allCircleViewModel.kt
@@ -1,4 +1,31 @@
 package com.example.techbook.ui
 
-class allCircleViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.techbook.data.CircleEntityConverter
+import com.example.techbook.data.api.entity.CircleEntity
+import com.example.techbook.data.api.service.CircleInterface
+import com.example.techbook.data.db.AppDatabase
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class AllCircleViewModel(
+    private val circleInterface: CircleInterface,
+    private val appDatabase: AppDatabase
+) : ViewModel() {
+
+    private val _allCircle = MutableLiveData<List<CircleEntity>>()
+    val allCircle: LiveData<List<CircleEntity>>
+        get() = _allCircle
+
+    fun fetchAllCircle() = viewModelScope.launch {
+        val result = circleInterface.circleAll().await()
+        _allCircle.postValue(result.result)
+    }
+
+    fun insertLikeCircle(circle: CircleEntity) = viewModelScope.launch {
+        appDatabase.userDao().insertNewData(CircleEntityConverter.toCircleDB(circle))
+    }
 }
